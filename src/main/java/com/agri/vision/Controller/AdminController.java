7b7ff9669cc2e26ff1bb79c3c1cf0e4d4edb18b6
siddharthk004 <siddharthkardile5@gmail.com>
2025-03-13 00:03:46 +0530

    new code Defalut Changes

--- src/main/java/com/agri/vision/Controller/AdminController.java
+++ src/main/java/com/agri/vision/Controller/AdminController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -95,7 +94,7 @@ public class AdminController {
                     category,
                     discount,
                     beforediscount,
-                    afterdiscount,
+                    afterdiscount, 
                     quantity);
             return ResponseEntity.ok(newProduct);
         } catch (Exception e) {
@@ -103,7 +102,7 @@ public class AdminController {
         }
     }
 
-    @PatchMapping("/admin/updateproduct/{id}")
+    @PostMapping("/admin/updateproduct/{id}")
     public ResponseEntity<?> updateProductFields(
             @PathVariable Long id,
             @RequestParam(value = "productimage", required = false) MultipartFile productimage,
